41a281d57bbcb27c57fcc0b7f36f96944161d428
bala-amypo <bala@amypo.in>
2025-12-21 17:11:49 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeliveryEvaluation.java
+++ src/main/java/com/example/demo/repository/DeliveryEvaluation.java
@@ -1,12 +1,14 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.DeliveryEvaluation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-public interface DeliveryEvaluationRepository extends JpaRepository<DeliveryEvaluation, Long> {
+public interface DeliveryEvaluationRepository
+        extends JpaRepository<DeliveryEvaluation, Long> {
 
-    List<DeliveryEvaluation> findByVendorld(Long vendorld);
+    List<DeliveryEvaluation> findByVendorId(Long vendorId);
 
-    List<DeliveryEvaluation> findBySlaRequirementld(Long slald);
+    List<DeliveryEvaluation> findBySlaRequirementId(Long slaId);
 }
