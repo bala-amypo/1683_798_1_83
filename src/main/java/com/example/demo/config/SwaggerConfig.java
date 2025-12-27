3cabe5f572f1bddbb65f28ae38927e706c2ff976
bala-amypo <bala@amypo.in>
2025-12-27 04:49:38 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,20 +1,38 @@
 package com.example.demo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
-public class OpenApiConfig {
+public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info()
+                        .title("Vendor SLA Performance Tracker API")
+                        .version("1.0")
+                        .description("Vendor SLA Performance Tracker with JWT Security"))
+
                 .servers(List.of(
-                        new Server().url("https://9277.408procr.amypo.ai/")
-                ));
-        }
+                        new Server().url("https://9011.pro604cr.amypo.ai/")
+                ))
+
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token in format: Bearer <token>")
+                        )
+                );
+    }
 }
