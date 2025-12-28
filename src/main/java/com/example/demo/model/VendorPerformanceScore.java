aa80277d470f1bc594b68e59d726399defa938e4
bala-amypo <bala@amypo.in>
2025-12-28 07:22:16 +0000

    User commit

--- src/main/java/com/example/demo/model/VendorPerformanceScore.java
+++ src/main/java/com/example/demo/model/VendorPerformanceScore.java
@@ -61,3 +61,89 @@
 //     }
 // }
 
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class VendorPerformanceScore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Vendor vendor;
+
+    private Double onTimePercentage;
+    private Double qualityCompliancePercentage;
+    private Double overallScore;
+
+    @ManyToOne
+    private VendorTier vendorTier;
+
+    private LocalDateTime calculatedAt = LocalDateTime.now();
+
+    public VendorPerformanceScore() {
+    }
+
+    public VendorPerformanceScore(Vendor vendor,
+                                  Double onTimePercentage,
+                                  Double qualityCompliancePercentage,
+                                  Double overallScore) {
+        this.vendor = vendor;
+        this.onTimePercentage = onTimePercentage;
+        this.qualityCompliancePercentage = qualityCompliancePercentage;
+        this.overallScore = overallScore;
+        this.calculatedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Vendor getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(Vendor vendor) {
+        this.vendor = vendor;
+    }
+
+    public Double getOnTimePercentage() {
+        return onTimePercentage;
+    }
+
+    public void setOnTimePercentage(Double onTimePercentage) {
+        this.onTimePercentage = onTimePercentage;
+    }
+
+    public Double getQualityCompliancePercentage() {
+        return qualityCompliancePercentage;
+    }
+
+    public void setQualityCompliancePercentage(Double qualityCompliancePercentage) {
+        this.qualityCompliancePercentage = qualityCompliancePercentage;
+    }
+
+    public Double getOverallScore() {
+        return overallScore;
+    }
+
+    public void setOverallScore(Double overallScore) {
+        this.overallScore = overallScore;
+    }
+
+    public VendorTier getVendorTier() {
+        return vendorTier;
+    }
+
+    public void setVendorTier(VendorTier vendorTier) {
+        this.vendorTier = vendorTier;
+    }
+
+    public LocalDateTime getCalculatedAt() {
+        return calculatedAt;
+    }
+}
